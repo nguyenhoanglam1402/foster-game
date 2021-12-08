c240b46bc72e6e472c411dba0ebee5aa9c6138ea
nguyenhoanglam1402 <nguyenhoanglam.emily@gmail.com>
2021-12-09 04:11:46 +0700

    feat(conversation): Movement for NPC and event trigger with Player

--- Assets/Script/ChoiceSystem.cs
+++ Assets/Script/ChoiceSystem.cs
@@ -4,43 +4,91 @@ using UnityEngine;
 using UnityEngine.UI;
 using Ink.Runtime;
 using TMPro;
+using UnityEngine.Windows.Speech;
 
 public class ChoiceSystem : MonoBehaviour
 {
-    [Header("Inker Script Source")]
-    [SerializeField]
+    [Header("Ink Script Source")]
     private Story story;
     [SerializeField]
-    private TextAsset textAsset;
-    [SerializeField]
-    private GameObject TextMessage;
-    [SerializeField]
+    private TextAsset script;
+    [Header("UI System")]
     private GameObject optionsUI;
-    [SerializeField]
     private Animator npcAnimator;
     [SerializeField]
-    private bool isMine;
+    private GameObject choiceButtonPrefab;
     [SerializeField]
-    private GameObject choiceButton;
+    private GameObject messagePanel;
+    private TextMeshProUGUI textMessage;
+    [SerializeField]
+    private GameObject dialogUIPrefab;
+    private GameObject dialogUI;
+    [SerializeField]
+    private GameObject pickButton;
+    [Header("Audio System")]
     [SerializeField]
     private AudioClip[] audioList;
+    private AudioSource audioSource;
+    [Header("Logic System")]
+    [SerializeField]
+    private bool isMine = false;
+    [Header("Personal of NPC Properties")]
+    [SerializeField]
+    private string fullname = "";
+    [SerializeField]
+    private string jobTitle = "";
 
     // Start is called before the first frame update
     void Start()
     {
         optionsUI = GameObject.FindGameObjectWithTag("Options");
         npcAnimator = transform.GetComponent<Animator>();
-        story = new Story(textAsset.text);
+        audioSource = GetComponent<AudioSource>();
+        messagePanel = GameObject.FindGameObjectWithTag("MessagePanel");
+        story = new Story(script.text);
         ContinueStory();
     }
 
-    // Update is called once per frame
-    void ContinueStory()
+	// Update is called once per frame
+	void ContinueStory()
     {
         if (story.canContinue && isMine)
         {
-            TextMessage.GetComponent<TextMeshPro>().text = story.Continue();
-            Debug.Log(story.canContinue);
+            string respond = story.Continue();
+            TextMeshProUGUI npcName = dialogUI.transform.Find("NPCName").GetComponent<TextMeshProUGUI>();
+
+            if (respond.Split('-').Length == 2)
+            {
+                int index = 0;
+                int indexAnimation = 0;
+				if (int.TryParse(respond.Split('-')[0], out index) == true)
+				{
+                    textMessage.text = respond.Split('-')[1];
+                    audioSource.PlayOneShot(audioList[index]);
+                    animationController(0);
+                }
+				else if (int.TryParse(respond.Split('-')[1], out indexAnimation) == true)
+				{
+                    textMessage.text = respond.Split('-')[0];
+                    animationController(indexAnimation);
+                }
+            }
+            else if (respond.Split('-').Length == 3)
+			{
+                int index = int.Parse(respond.Split('-')[0]);
+                int indexAnimation = int.Parse(respond.Split('-')[2]);
+                textMessage.text = respond.Split('-')[1];
+                audioSource.PlayOneShot(audioList[index]);
+                animationController(indexAnimation);
+            }
+			else
+			{
+                textMessage.text = respond;
+                animationController(0);
+            }
+            
+            Debug.Log("Debug: " + story.canContinue + " | Story length: "+ respond.Split('-').Length);
+            Debug.Log("Respond:[" + respond + "]");
         }
         RefreshChoice();
     }
@@ -50,7 +98,7 @@ public class ChoiceSystem : MonoBehaviour
         clearUI();
         foreach (Choice choice in story.currentChoices)
         {
-            GameObject buttonObject = Instantiate(choiceButton, optionsUI.transform);
+            GameObject buttonObject = Instantiate(choiceButtonPrefab, optionsUI.transform);
             buttonObject.GetComponentInChildren<Text>().text = choice.text;
             Button buttonComponent = buttonObject.GetComponent<Button>();
             buttonComponent.onClick.AddListener(() => ChooseAnswer(choice.index));
@@ -73,17 +121,59 @@ public class ChoiceSystem : MonoBehaviour
 		{
             Debug.Log("Click on " + index);
             story.ChooseChoiceIndex(index);
-            npcAnimator.SetTrigger("Conversation");
             ContinueStory();
         }
 	}
+
+    void animationController(int indexAnimation)
+	{
+        switch (indexAnimation)
+        {
+            case 0:
+                npcAnimator.SetTrigger("Yes");
+                break;
+            case 1:
+                npcAnimator.SetTrigger("No");
+                break;
+            case 2:
+                npcAnimator.SetTrigger("Loose");
+                break;
+            case 3:
+                npcAnimator.SetTrigger("Shrug");
+                break;
+            case 4:
+                npcAnimator.SetTrigger("Wave");
+                break;
+            case 5:
+                npcAnimator.SetTrigger("Win");
+                break;
+            case 6:
+                npcAnimator.SetTrigger("Dance");
+                break;
+            default:
+                break;
+        }
+    }
+
+    void ShowDialogUI () {
+        dialogUI = Instantiate(dialogUIPrefab, messagePanel.transform);
+        textMessage = dialogUI.transform.Find("Message").GetComponent<TextMeshProUGUI>();
+        dialogUI.transform.Find("NPCName").GetComponent<TextMeshProUGUI>().text = fullname;
+        dialogUI.transform.Find("NPCJobTitle").GetComponent<TextMeshProUGUI>().text = jobTitle;
+        ContinueStory();
+    }
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if(other.tag == "Player")
 		{
             isMine = true;
-            ContinueStory();
+            GameObject button = Instantiate(pickButton, optionsUI.transform);
+            button.GetComponentInChildren<TextMeshProUGUI>().text = "Talk with " + fullname;
+            button.GetComponent<Button>().onClick.AddListener(()=> {
+                ShowDialogUI();
+                Destroy(button.gameObject);
+            });
 		}
 	}
 
@@ -94,6 +184,8 @@ public class ChoiceSystem : MonoBehaviour
             isMine = false;
             clearUI();
             Debug.Log("Hi I'm out!");
-		}
+            story.ResetState();
+            Destroy(dialogUI);
+        }
 	}
 }
--- Assets/USScene/Script/Character/Player/PlayerController.cs
+++ Assets/USScene/Script/Character/Player/PlayerController.cs
@@ -17,12 +17,19 @@ public class PlayerController : MonoBehaviour
     private float turnSmoothTime = 0.1f;
     [SerializeField]
     private Transform mainCamera;
+    [SerializeField]
+    private Transform isGroundCheckSphereSensor;
+    [SerializeField]
+    private float radiusCheckSphere = 5f;
+    [SerializeField]
+    private LayerMask grounds;
     // Start is called before the first frame update
     void Start()
     {
         animatorController = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
         animatorController.SetBool("Grounded", true);
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
     }
 
     // Update is called once per frame
@@ -31,12 +38,14 @@ public class PlayerController : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
-        if(direction.magnitude >= 0.1f)
+
+        if (direction.magnitude >= 0.1f)
 		{
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;            
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Debug.Log(isGround());
             characterController.Move(moveDirection.normalized * speed * Time.deltaTime);
             animatorController.SetFloat("MoveSpeed", 1);
 		}
@@ -44,5 +53,18 @@ public class PlayerController : MonoBehaviour
 		{
             animatorController.SetFloat("MoveSpeed", 0);
 		}
+
+        if (!isGround() && direction.y < 0)
+        {
+            direction.y += Physics.gravity.y * Time.deltaTime;
+            Debug.Log(isGround());
+            characterController.Move(direction * Time.deltaTime);
+        }
     }
+
+    bool isGround()
+	{
+        return Physics.CheckSphere(isGroundCheckSphereSensor.position, radiusCheckSphere, grounds);
+    }
+
 }
